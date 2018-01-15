81bbb0d4581339ce9cc7300330d8701c509df6cd
yogurtkun <yogurtkun@126.com>
2018-01-15 18:57:58 -0500

    meogame start

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import { canvas } from "./libs/weapp-adapter";
+
+//let ctx = canvas.getContext('2d')
+
+export default class Main{
+    constructor(){
+        this.restart();
+    }
+
+    restart(){
+        console.log("hello world!\n");
+    }
+}
