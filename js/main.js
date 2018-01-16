e9ad785aa674a9b1687c931dadb07e0dc96594ff
yogurtkun <yogurtkun@126.com>
2018-01-16 10:59:03 -0500

    start to loop

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
-import { canvas } from "./libs/weapp-adapter";
+import Background from "./runtime/background";
 
-//let ctx = canvas.getContext('2d')
+let ctx = canvas.getContext('2d')
 
 export default class Main{
     constructor(){
         this.restart();
     }
 
+    update(){
+        this.bg.update(ctx);
+        console.log("Hello world!");
+    }
+
+    loop(){
+        this.update();
+
+        window.requestAnimationFrame(
+            this.loop.bind(this),
+            canvas
+          )
+    }
+
     restart(){
-        console.log("hello world!\n");
+        this.bg = new Background();
+        this.bg.render(ctx);
+
+        window.requestAnimationFrame(
+            this.loop.bind(this),
+            canvas
+          )
     }
 }
---
+++ js/runtime/background.js
@@ -0,0 +1,39 @@
+const screenWidth  = window.innerWidth
+const screenHeight = window.innerHeight
+
+const BG_IMG_SRC   = 'images/bg.jpg'
+const BG_WIDTH     = 512
+const BG_HEIGHT    = 512
+
+export default class Background{
+    constructor(){
+        this.img = new Image();
+        this.img.src = BG_IMG_SRC;
+        this.top = 0;
+    }
+
+    update(ctx){
+        this.top += 2;
+        if(this.top > window.innerHeight){
+            this.top = 0;
+        }
+        this.render(ctx);
+    }
+
+    render(ctx){
+        ctx.drawImage(
+            this.img,
+            0,
+            -window.innerHeight + this.top,
+            window.innerWidth,
+            window.innerHeight
+        )
+        ctx.drawImage(
+            this.img,
+            0,
+            this.top,
+            window.innerWidth,
+            window.innerHeight
+        )
+    }
+}
